c783834a3c174846951a0e226e22dd83040bdea1
spidernik84 <11486215+spidernik84@users.noreply.github.com>
2025-12-05 21:37:13 +0100

    update radioparadise script

--- dotfiles/sway/scripts/rpstatus.sh
+++ dotfiles/sway/scripts/rpstatus.sh
@@ -1,1 +1,4 @@
+### Print the Radioparadise song currently playing on the main mix channel, plus show the cover in kitty.
+### TODO: use single curl call
 curl -s 'https://api.radioparadise.com/api/nowplaying_list_v2022' | jq -r ".song[0] | \"Artist: \(.artist), Title: \(.title), Year: \(.year)\""
+kitten icat --align left https://img.radioparadise.com/$(curl -s 'https://api.radioparadise.com/api/nowplaying_list_v2022' | jq -r ".song[0].cover")
